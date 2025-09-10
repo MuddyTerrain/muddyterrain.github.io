3c8f9681dfc5c33461b740c2165a7badf337953f
Prajwal Shetty <prajwalshetty3000@gmail.com>
2025-09-10 12:00:50 +0100

    Fix: Resolve JS error breaking Google Analytics

--- assets/js/internal-traffic.js
+++ assets/js/internal-traffic.js
@@ -15,15 +15,3 @@ document.addEventListener('DOMContentLoaded', function() {
     document.cookie = internalTrafficCookieName + "=true;" + expires + ";path=/";
   }
 });
-
-// Function to get a cookie
-function getCookie(name) {
-  const nameEQ = name + "=";
-  const ca = document.cookie.split(';');
-  for(let i=0; i < ca.length; i++) {
-    let c = ca[i];
-    while (c.charAt(0) === ' ') c = c.substring(1, c.length);
-    if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
-  }
-  return null;
-}
